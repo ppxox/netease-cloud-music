58d83ce5d79328e793c92a6d90cc8ec6e10fa53a
didadi599 <1041288269@qq.com>
2019-03-05 20:30:50 +0800

    布局

--- src/views/MusicHome/components/HotRecommend.vue
+++ src/views/MusicHome/components/HotRecommend.vue
@@ -2,7 +2,12 @@
   <div class="hot-recommend">
     <TabBar :title="title" :path="path" :tabList="tabList"></TabBar>
     <ul class="recommend-list">
-      <li class="item"></li>
+      <li class="item">
+        <div class="img-wrap">
+          <img src="http://p1.music.126.net/D-TvG1LXc8mx_wvaHVVUjQ==/109951163903856167.jpg?param=140y140" class="auto-img">
+          <router-link to="/" class="link-wrap"></router-link>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -54,6 +59,29 @@ export default {
 
   .recommend-list {
     margin: 20px 0 0 -42px;
+  }
+
+  .item {
+    float: left;
+    width: 140px;
+    height: 204px;
+    overflow: hidden;
+    padding: 0 0 30px 0;
+    margin-left: 42px;
+  }
+
+  .img-wrap {
+    width: 140px;
+    height: 140px;
+    position: relative;
+  }
+
+  .link-wrap {
+    position: absolute;
+    top: 0;
+    left: 0;
     width: 100%;
+    height: 100%;
+    background: url('../../../../public/img/coverall.png') no-repeat;
   }
 </style>
